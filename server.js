3c7a6d02b613e4e99c242f5a9e053c5d8949444a
Pritesh Yadav <priteshyadav2015@gmail.com>
2021-09-05 22:53:36 +0530

    Cors

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const file = require('./routes/file')
 const path = require("path")
 const cors = require('cors')
 
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+
+
+app.use(cors(corsOptions))
 app.use(express.static(path.join("public")))
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
@@ -17,11 +25,6 @@ app.use("/files/", require("./routes/showfile"))
 app.use("/files/download/", require("./routes/download"))
 
 
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-app.use(cors(corsOptions))
 
 connectDb();
 app.listen(PORT, () => {
